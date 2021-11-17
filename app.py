20143b1da40746feff3eea66f5db48383ed9ca3f
jinmook <jink0608@naver.com>
2021-11-17 23:05:29 +0900

    Book_list 테이블에 데이터 저장

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from views import main_view
-from models import *
-
 
 app = Flask(__name__)
 app.register_blueprint(main_view.bp)
 
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/elice-library'
 
 
 if __name__ == '__main__':
