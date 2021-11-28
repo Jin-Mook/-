d1aacf258e621ba28349396f1b11adda863efe52
jinmook <jink0608@naver.com>
2021-11-28 16:17:55 +0900

    최종 파일

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from models import db
 from views import main_view, book_info_view, book_checkout_return_view
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(main_view.bp)
 app.register_blueprint(book_info_view.bp)
 app.register_blueprint(book_checkout_return_view.bp)
 app.secret_key = 'secretttt'
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/elice_library'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -16,4 +17,4 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host="0.0.0.0")
